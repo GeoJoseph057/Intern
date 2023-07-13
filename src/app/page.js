9421a5f5199cec24c94ce20fcd94ab859c5a0bb5
Geo Joseph <geojoseph.25ec@licet.ac.in>
2023-07-13 15:52:49 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,31 @@
+"use client"
+import { useState } from "react";
+import { Configuration, OpenAIApi } from "openai";
+
+export default function Home() {
+  const [imageUrl, setImageUrl] = useState("");
+
+  const createImg = async () => {
+    const configuration = new Configuration({
+      apiKey: "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",
+    });
+    const openai = new OpenAIApi(configuration);
+
+      const response = await openai.createImage({
+        prompt: "a white siamese cat",
+        n: 1,
+        size: "256x256",
+      });
+
+      setImageUrl(response.data.data[0].url);
+
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>image url:{imageUrl}</div>
+   <img src={imageUrl} width={1000} height={1000}/>
+      <button onClick={createImg}><div>click me</div></button>
+    </main>
+  );
+}
